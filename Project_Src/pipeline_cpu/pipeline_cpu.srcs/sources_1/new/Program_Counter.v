b6a086a5e80964d32753b97aa5dce4ebb9b44f35
Zarror <918670734@qq.com>
2021-08-30 11:59:30 +0800

    添加相关文件

---
+++ Project_Src/pipeline_cpu/pipeline_cpu.srcs/sources_1/new/Program_Counter.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/08/30 10:52:24
+// Design Name: 
+// Module Name: Program_Counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Program_Counter(
+input wire I_From
+
+    );
+endmodule
