fc901a59ac0a3f759ed64bfba4a835150fcfe70e
WXZ <1790828743@qq.com>
2021-09-07 15:43:21 +0800

    ex-添加lw部分

--- test/mips2.asm
+++ test/mips2.asm
@@ -1,15 +1,11 @@
 lui $t1,4
 lui $t2,5
 lui $t3,6
-or $t1,$t2,$t3
-and $t1,$t2,$t3
-xor $t1,$t2,$t3
-nor $t1,$t2,$t3
-ori $t1,$t2,100
-andi $t1,$t2,100
-xori $t1,$t2,100
-
-sllv $t1,$t2,$t3
+or $t1,$t2,100		;t1 = 0005 0064
+and $t1,$t2,100		;t1 = 00000000
+xor $t1,$t2,100 	;t1 = 0005 0064
+nor $t1,$t2,$t3 	;t1 = fff8 ffff
+sllv $t1,$t2,$t3 	;t1 = 
 srlv $t1,$t2,$t3
 movn $t1,$t2,$t3
 slt  $t1,$t2,$t3
